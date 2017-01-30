8d5ad092c4bb65322f25c52a853afd7bc5fc5058
Djordje Stojiljkovic <djordjewebdizajn@gmail.com>
2017-01-30 02:07:40 +0100

    mb_substr fix

--- lib/ForceCMS/Vendors/Gajus/Dindent/Indenter.php
+++ lib/ForceCMS/Vendors/Gajus/Dindent/Indenter.php
@@ -124,8 +124,8 @@ class Indenter
                         'match' => $matches[0]
                     );
 
-                    $subject = mb_substr($subject, mb_strlen($matches[0]));
-
+                    $subject = substr($subject, mb_strlen($matches[0]));
+                    // TODO: push back to mb_substr for local app
                     if ($rule === static::MATCH_DISCARD) {
                         break;
                     }
